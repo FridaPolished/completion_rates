36abddfe7222bf8c9e76a7058ef4995411d622c4
FridaPolished <fridapulido@gmail.com>
2019-11-20 10:40:21 -0800

    Add zoom feature to map

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Completion Rates
+# UNESCO School Completion Rates 2019
+## Upper-secondary Education
 
 ### Overview
 
   Completion rates provides a data visualization for the completion rates from upper secondary education around the world.
 
-All data from "Completion rates"(November 2019) by [Unicef]('https://data.unicef.org/resources/dataset/education-data/')
+All data from "Completion rates"(November 2019) by [Unicef](https://data.unicef.org/resources/dataset/education-data/)
 
 ### MVP
   * Format the data.
@@ -26,9 +27,12 @@ All data from "Completion rates"(November 2019) by [Unicef]('https://data.unicef
   * Day 2
     * Build on geoJSON file.
     * Build elements in DOM to render map.
+    * Add tooltips to display general information.
   * Day 3 
     * Continue building elements in DOM to render map.
+    * Add new versions of the map to filter results by specific data.
+    * Add color bar to explain color range.
   * Day 4
-    * Add styling and introduction description.
+    * Add styling and introduction description paragraph on completion rates concept.
   * Day 5 
     * Finishing touches and presentation
--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var svg = d3.select("svg"),
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
+let svg = d3.select("#container")
+.append('svg')
+.attr("width", 800)
+.attr("height", 800)
+.call(d3.zoom().on("zoom", function(){
+  svg.attr("transform", d3.event.transform)
+}))
+.append('g')
+
 
 Promise.all([
   d3.json('data.json'),
   d3.json('countries.geo.json')
-]).then(showData)
+]).then(showData);
+
+
 
 function showData(datasources) {
   let data = datasources[0];
@@ -39,6 +47,8 @@ function showData(datasources) {
   let projection = d3.geoNaturalEarth1()
     .scale(120)
     .translate([bodyWidth / 2, bodyHeight / 2])
+
+  
 
   let path = d3.geoPath()
     .projection(projection)
@@ -86,4 +96,69 @@ function showData(datasources) {
     .text(function (d) {
       if (d.properties.total) { return d.properties.name + ' ' + d.properties.total }
     })
+    .attr("class", "title")
 }
+
+// Promise.all([
+//   d3.json('data.json'),
+//   d3.json('countries.geo.json')
+// ]).then(genderData)
+
+function genderData(datasources){
+    let data = datasources[0];
+    let mapInfo = datasources[1];
+    let bodyHeight = 800;
+    let bodyWidth = 800;
+
+
+    let dataIndex = {};
+    for (let i = 0; i < data.length; i++) {
+      let c = data[i];
+      let country = c.country;
+      dataIndex[country] = c.female
+    }
+
+    mapInfo.features = mapInfo.features.map(function (d) {
+      let country = d.properties.name
+      let female = dataIndex[country]
+      d.properties.female = female;
+      return d;
+    })
+
+    let maxTotal = d3.max(mapInfo.features, function (d) { return d.properties.female });
+    let median = d3.median(mapInfo.features, function (d) { return d.properties.female });
+
+
+
+    let colorScale = d3.scaleLinear()
+      .domain([0, median, maxTotal])
+      .range(["yellow", "green", "dark-green"]);
+
+
+
+    let projection = d3.geoNaturalEarth1()
+      .scale(120)
+      .translate([bodyWidth / 2, bodyHeight / 2])
+
+    let path = d3.geoPath()
+      .projection(projection)
+
+    svg.selectAll("path").data(mapInfo.features)
+      .enter().append("path")
+      .attr("d", function (d) { return path(d) })
+      .attr("stroke", "gray")
+      .attr("fill",
+        function (d) {
+          if (d.properties.female) {
+            return colorScale(d.properties.female)
+          }
+          else {
+            return "white"
+          }
+        })
+      .append('title')
+      .text(function (d) {
+        if (d.properties.total) { return d.properties.name + ' ' + d.properties.female }
+      })
+      .attr("class", "title")
+  }
