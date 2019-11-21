5aff89b097c56c79f98d9e3c144f210e79cba352
FridaPolished <fridapulido@gmail.com>
2019-11-21 14:20:05 -0800

    Add color scale information

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+let width= 800;
+let height = 800;
+let svgDx = 100;
+let svgDy = 100;
+
 let svg = d3.select("#container")
 .append('svg')
-.attr("width", 800)
-.attr("height", 800)
+.attr("width", width)
+.attr("height", height)
 .call(d3.zoom().on("zoom", function(){
   svg.attr("transform", d3.event.transform)
 })
-  .scaleExtent([1, 8])
-  )
+.scaleExtent([1, 6])
+.translateExtent([[svgDx, svgDy], [width -(svgDx * 2) ,height-svgDy]]))
 .append('g')
 
 
 function showData(datasources, value) {
-
-  console.log(value)
   let data = datasources[0];
   let mapInfo = datasources[1];
   let bodyHeight = 800;
@@ -35,27 +38,32 @@ function showData(datasources, value) {
 
   let maxTotal = d3.max(mapInfo.features, function (d) { return d.properties[value] });
   let median = d3.median(mapInfo.features, function (d) { return d.properties[value]});
-
+  let min = d3.min(mapInfo.features, function (d) { return d.properties[value]});
+  
   let colorScale; 
   if(value === 'total'){
     colorScale = d3.scaleLinear()
-      .domain([0, median, maxTotal])
+      .domain([min, median, maxTotal])
       .range(["yellow", "orange", "red"]);
+    appendLegend(min, median, maxTotal, "yellow", "orange", "red" )
   } else if (value === 'female' || value === 'male' ){
     colorScale = d3.scaleLinear()
-      .domain([0, median, maxTotal])
-      .range(["rgb(233, 242, 205)", 'rgb(212, 230, 160)', "rgb(108, 122, 67)"]);
+    .domain([0, median, maxTotal])
+    .range(["rgb(233, 242, 205)", 'rgb(212, 230, 160)', "rgb(108, 122, 67)"]);
+    appendLegend(min, median, maxTotal, "rgb(233, 242, 205)", 'rgb(212, 230, 160)', "rgb(108, 122, 67)" )
   } else if (value === "rural" || value === 'urban'){
-      colorScale = d3.scaleLinear()
-      .domain([0, median, maxTotal])
-      .range(["rgb(229, 219, 255)", "rgb(159, 131, 242)", "rgb(77, 42, 217)"]);
-    } else {
-      colorScale = d3.scaleLinear()
-      .domain([0, median, maxTotal])
-      .range(["rgb(250, 208, 223)", "rgb(242, 62, 134)", "rgb(176, 96, 125)"]);
+    colorScale = d3.scaleLinear()
+    .domain([0, median, maxTotal])
+    .range(["rgb(229, 219, 255)", "rgb(159, 131, 242)", "rgb(77, 42, 217)"]);
+    appendLegend(min, median, maxTotal, "rgb(229, 219, 255)", "rgb(159, 131, 242)", "rgb(77, 42, 217)" )
+  } else {
+    colorScale = d3.scaleLinear()
+    .domain([0, median, maxTotal])
+    .range(["rgb(250, 208, 223)", "rgb(242, 62, 134)", "rgb(176, 96, 125)"]);
+    appendLegend(min, median, maxTotal, "rgb(250, 208, 223)", "rgb(242, 62, 134)", "rgb(176, 96, 125)" )
   }
 
-  
+
 
 
   let projection = d3.geoNaturalEarth1()
@@ -95,29 +103,41 @@ function showData(datasources, value) {
     .enter().append("path")
     .attr("d", function (d) { return path(d) })
     .attr("stroke", "gray")
+    .attr("id", "country")
     .attr("fill",
       function (d) {
         if (d.properties[value]) {
           return colorScale(d.properties[value])
         }
         else {
-          return "white"
+          return "rgb(230, 230, 230)"
         }
       })
     .on("mouseleave", mouseLeave)
     .on("mouseover", mouseOver)
     .append('title')
     .text(function (d) {
-      if (d.properties[value]) { return d.properties.name + ' ' + d.properties[value]}
-    })
-    .attr("class", "title")
-}
+        if (d.properties[value]) { return d.properties.name + ' ' + d.properties[value]}
+      })
+      .attr("id", "title")
+  }
 
-  
+
+  function appendLegend(min, median, max, color1, color2, color3){
+    svg.append("circle").attr("cx", 10).attr("cy", 130).attr("r", 6).style("fill", `${color3}`).attr('class', "legend").attr("position", "absolute")
+    svg.append("circle").attr("cx", 10).attr("cy", 160).attr("r", 6).style("fill", `${color2}`).attr('class', "legend")
+    svg.append("circle").attr("cx", 10).attr("cy", 190).attr("r", 6).style("fill", `${color1}`).attr('class', "legend")
+    svg.append("text").attr("x", 20).attr("y", 130).text(`Maximum ${max}`).style("font-size", "15px").attr("alignment-baseline", "middle").attr('class', "legend")
+    svg.append("text").attr("x", 20).attr("y", 160).text(`Median ${median}`).style("font-size", "15px").attr("alignment-baseline", "middle").attr('class', "legend")
+    svg.append("text").attr("x", 20).attr("y", 190).text(`Minimum ${min}`).style("font-size", "15px").attr("alignment-baseline", "middle").attr('class', "legend")
+  }
+
+
 
   function displayMap(filter) {
     d3.selectAll("path").remove()
-    console.log(filter)
+    d3.selectAll("circle").remove()
+    d3.selectAll("text").remove()
     switch(filter){
       case 'total':
         Promise.all([
